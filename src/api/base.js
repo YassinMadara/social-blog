8dc2dff72c685e6757d21bbc8a82204b699eaceb
YassinMadara <YassinMadara@gmail.com>
2024-02-01 23:48:10 +0300

    changing index.html file

--- src/api/base.js
+++ src/api/base.js
@@ -3,4 +3,5 @@ import axios from "axios";
 export const baseApi = axios.create({
   // baseURL: import.meta.env.VITE_API,
   baseURL: "https://json-api-psi-two.vercel.app",
+  // headers: { "Content-Type": "application/JSON" },
 });
