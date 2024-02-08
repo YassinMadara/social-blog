2c41c49442aff495fb064301eea45cac4d5c9c05
YassinMadara <YassinMadara@gmail.com>
2024-02-08 21:28:31 +0300

    .

--- src/router.jsx
+++ src/router.jsx
@@ -25,7 +25,7 @@ export const router = createBrowserRouter([
     element: <ParentLayout />,
     children: [
       {
-        errorElement: <Navigate to="posts" />,
+        errorElement: <Navigate to="/" />,
         children: [
           { index: true, element: <Navigate to="posts" /> },
           { path: "*", element: <UnvalidURLPage /> },
